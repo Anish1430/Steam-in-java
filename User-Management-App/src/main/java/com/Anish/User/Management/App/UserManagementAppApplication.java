d15ab01b3e57beaa6b7b65520922900ab31d41c3
Anish Kumar Sharma <anish.kumar20071998@gmail.com>
2023-09-14 15:27:32 +0530

    "Anish is creating"

--- User-Management-App/src/main/java/com/Anish/User/Management/App/UserManagementAppApplication.java
+++ User-Management-App/src/main/java/com/Anish/User/Management/App/UserManagementAppApplication.java
@@ -8,6 +8,7 @@ public class UserManagementAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserManagementAppApplication.class, args);
+		System.out.println("Anish is Creating an App");
 	}
 
 }
